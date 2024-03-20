481c4bc20533c0dfcd9c07c83943735b9fe5f129
Abhishek yadav <aky732244@gmail.com>
2024-03-20 14:07:14 +0530

    a

---
+++ 01basics/hi.js
@@ -0,0 +1,29 @@
+"use strict"; //for treating all js code as newer version but most of editior it's already build.
+
+/*BASIC COMMANDS*/
+
+console.log("hello world"); //console.log use for print
+
+// alert(3+3)
+
+// console.log(3+3) console.log("jhjhdb") it's should be readable to other. bad readablitity
+
+/* DATATYPE BASIC*/
+
+// string,number,boolean,bigint,null=standalone value,undefined=not,symbol=unique,object ......
+
+let name="abhishek" // in this case let become sting datatype
+
+let age =20 //let become number
+
+let islogin=true //let become boolean type
+
+let temp=null; //null is object null is unique 
+
+let height; // it's undefine datatype
+
+console.log(typeof height)
+
+/*conversion of datatype*/
+
+
