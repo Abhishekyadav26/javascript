113ec2720c355f65156cb2f5c40f07e6e9a96f63
Abhishekyadav26 <aky732244@gmail.com>
2024-08-08 16:36:39 +0530

    updated promises.js

--- promises/promises.js
+++ promises/promises.js
@@ -26,11 +26,12 @@ const promisethree = new Promise(function(resolve,reject){
     },1000)
 })
 promisethree.then(function(userdetails){
-    console.log(userdetails);
+    // console.log(userdetails);
 })
 // -------------------------------------------------
 
-const promisesfour = Promise(function(resolve,reject){
+
+const promisesfour = new Promise(function(resolve,reject){
     setTimeout(function(){
         let error = false;
         if(!error){
@@ -39,4 +40,66 @@ const promisesfour = Promise(function(resolve,reject){
             reject('ERROR : something went wrong')
         }
     },1000)
+})
+// promisesfour.then().catch()
+promisesfour
+.then(function(user){
+    // console.log(user);
+    return user.username
+},1000)
+.then((username)=>{
+    // console.log(username);
+})
+.catch((error)=>{
+    // console.log(error);
+})
+.finally(()=>{
+    // console.log("promises is resolved");
+})
+// ---------------------------------------------------------
+
+const promisefive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username:"javascript",email:"js@example.com"})
+        }else{
+            reject('ERROR : JS went wrong')
+        }
+    },1000)
+})
+
+async function consumefive(){
+    try{
+        const response = await promisefive
+        // console.log(response);
+    }catch(error){
+        // console.log(error);
+    } 
+}
+
+consumefive()
+
+//------------------------------------------------------------
+
+// async function getalluserdata(){
+//     try {
+//         const response = await fetch('https://api.github.com/users/abhishekyadav26')
+//         const data = await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: error");
+//     }
+// }
+// ---------------------------------------------------------------------------
+
+fetch('https://api.github.com/users/abhishekyadav26')
+.then(function(response){
+    return response.json();
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log(error);
 })
